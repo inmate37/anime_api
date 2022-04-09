6115baef0139249d97d1313e3a0def2455027bd6
Madi7 <madi7maratovic@gmail.com>
2022-04-09 21:51:11 +0600

    Fast commit

--- apps/auths/views.py
+++ apps/auths/views.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 # from django.db.models import QuerySet
 
@@ -12,14 +13,14 @@ class ListUsers(APIView):
     """
     View for CustomUser.
 
-    * Does-not equire token authentication.
+    * Does-not require token authentication.
     * Only superusers are able to access this view.
     """
     permission_classes: tuple = (
         permissions.IsAdminUser,
     )
 
-    def get(self, request) -> Response:
+    def get(self, request: Request) -> Response:
         """Return list of all users."""
 
         data: list = [
